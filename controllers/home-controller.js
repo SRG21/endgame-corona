0a26a0397e774c55a56c8e2499dc76518736bde0
Sankalp Rai Gambhir <sankalp_rai_gambhir@yahoo.com>
2020-04-23 01:04:05 +0530

    Third commit: controller setup

---
+++ controllers/home-controller.js
@@ -0,0 +1,3 @@
+module.exports.home = function(req,res){
+    return res.end('<h1>To Do List Project</h1>')
+}
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
-console.log("Router Loading....................");
+console.log("Router Loading......................");
 
 const express = require('express'); // fetches the existing instance of express
 const router = express.Router();
+const homeController = require('../controllers/home-controller');
 
 console.log("Router Loaded.");
+
+router.get('/', homeController.home);
 
 module.exports = router;
