7add81f511bca93daca481649b04fdd826b5c350
Sankalp Rai Gambhir <sankalp_rai_gambhir@yahoo.com>
2020-04-23 00:36:21 +0530

    Second Commit: Route setup

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+//Using Express Router
+
+app.use('/', require('./routes')); // it will by default fetch index
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error is setting up the server: ${err}`);
---
+++ routes/index.js
@@ -0,0 +1,8 @@
+console.log("Router Loading....................");
+
+const express = require('express'); // fetches the existing instance of express
+const router = express.Router();
+
+console.log("Router Loaded.");
+
+module.exports = router;
